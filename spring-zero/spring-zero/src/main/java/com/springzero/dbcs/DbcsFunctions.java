846caa8a2fabe3432fd56ed9caffa18a7f8fb93f
ntthuat <nguyenthienthuat92@gmail.com>
2017-05-28 18:16:26 +0700

    05/28/2017 commit tìm hiểu dbcs ở công ty

--- spring-zero/spring-zero/src/main/java/com/springzero/dbcs/DbcsFunctions.java
+++ spring-zero/spring-zero/src/main/java/com/springzero/dbcs/DbcsFunctions.java
@@ -1,5 +1,7 @@
 package com.springzero.dbcs;
 
+import java.nio.charset.Charset;
+
 /**
  * Class này dùng để viết các method về DBCS (Double Byte CharacterS Set).
  * 
@@ -32,5 +34,56 @@ public class DbcsFunctions {
 	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
 	    }
 	    return new String(hexChars);
+	}
+	
+	/**
+	 * in MS932 (CP932), <br>
+	 * Lead-Byte Ranges is 0x81 - 0x9F & 0xE0 - 0xFC<br>
+	 * Trail-Byte Ranges is 0x40 - 0xFC(except 0x7F)<br>
+	 * 
+     * @param c - char
+     * @return - true if it's in the leading byte range
+	 */
+    public static boolean isDbcsLeadingChar(char c) {
+    	return c >= 0x81 && c <= 0x9F || c >= 0xE0 && c <= 0xFC;
+    }
+
+    /**
+     * 
+	 * in MS932 (CP932), <br>
+	 * Trail-Byte Ranges is 0x40 - 0xFC(except 0x7F)<br>
+     * @param c - char
+     * @return - true if it's in the trail byte range
+     */
+    public static boolean isTrailingChar(char c) {
+    	return c >= 0x40 && c <= 0xFC && c != 0x7F;
+    }
+    
+    /**
+	 * Convert from <b>source</b> charset to <b>target</b> charset<br>
+	 * The purpose of doing this is to allow customised conversion in relate to EBCDIC as it might be platform/jvm
+	 * dependant result, which is not desirable. It would be safer to have a fixed way of converting from EBCDIC to/from
+	 * ASCII.
+	 *
+	 * Ví dụ: từ windows-31j sang ISO-8859-1
+	 *
+	 * @param str - String to be converted, which is assumed not null and encoded in <b>source</b> charset
+	 * @param fromCharset - source charset that the String is encoded
+	 * @param toCharset - target charset to be encoded to
+	 * @return String encoded in <b>target</b> character set
+	 */
+	public static String convertCharset(final String str, final Charset fromCharset, final Charset toCharset) {
+		return fromCharset.equals(toCharset) ? str : new String(str.getBytes(fromCharset), toCharset);
+	}
+	
+	/**
+	 * Convert String literal, assumed in unicode, to be single byte String (e.g. SHIFT-JIS for Japanese characters).<br>
+	 * Hình như windows-31j là MS932
+	 * 
+	 * @param s - String contains unicode characters
+	 * @return s that have DBCS into single bytes
+	 */
+	public static String fromDbcs(final String s) {
+		return DbcsFunctions.convertCharset(s, Charset.forName("windows-31j"), Charset.forName("ISO-8859-1"));
 	}
 }
--- spring-zero/spring-zero/src/test/java/com/springzero/dbcs/DbcsTest.java
+++ spring-zero/spring-zero/src/test/java/com/springzero/dbcs/DbcsTest.java
@@ -13,7 +13,6 @@ import org.apache.commons.codec.binary.Hex;
 
 import org.junit.Test;
 
-
 /**
  * Class này dùng để chạy thử các ví dụ về DBCS (Double Byte CharacterS Set)
  * 
@@ -21,26 +20,29 @@ import org.junit.Test;
  * @version 05/25/2017
  */
 public class DbcsTest {
-	
+
 	/**
 	 * Class này dùng để chạy thử các ví dụ về DBCS.<br>
-	 * @throws DecoderException 
+	 * 
+	 * @throws DecoderException
 	 */
 	@Test
-	public void TestExample() throws DecoderException{
-		String str = "大";
+	public void TestExample() throws DecoderException {
+		String str = "a昨大昨";
 		char a = '大';
+		System.out.println(str.charAt(1));
+		System.out.println(a);
 		System.out.println(str.getBytes().length);
 		Charset EBCDIC_CHARSET = Charset.forName("cp037");
 		Charset encoding = Charset.forName("cp037");
 		boolean b = EBCDIC_CHARSET.contains(encoding);
 		String codePage = "abcdef";
-		
+
 		// tạo ra nativeChars là bảng mã ASCII
 		final char[] nativeChars = new char[256];
 		for (int i = 0; i < nativeChars.length; i++) {
-        	nativeChars[i] = (char) i;
-        }
+			nativeChars[i] = (char) i;
+		}
 		String hex = DbcsFunctions.bytesToHex("大".getBytes());
 		String space1byte = " ";
 		System.out.println(new String(Hex.encodeHex(space1byte.getBytes())));
@@ -49,7 +51,15 @@ public class DbcsTest {
 	}
 	
 	@Test
-	public void testSpace2Bytes(){
+	public void testFromDbcs(){
+		String str = DbcsFunctions.fromDbcs("a昨夜のコンサートは最高でした");
+		assertEquals("aðéÌRT[gÍÅÅµ½", str);
+		String str2 = DbcsFunctions.convertCharset(str, Charset.forName("ISO-8859-1"), Charset.forName("windows-31j"));
+		assertEquals("a昨夜のコンサートは最高でした", str2);
+	}
+	
+	@Test
+	public void testSpace2Bytes() {
 		String space1Byte = " ";
 		String result = new String(Hex.encodeHex(space1Byte.getBytes()));
 		assertEquals("20", result);
@@ -57,23 +67,58 @@ public class DbcsTest {
 		result = new String(Hex.encodeHex(space2Bytes.getBytes()));
 		assertEquals("e38080", result);
 	}
+
+	@Test
+	public void testCharToHeximal(){
+		assertEquals("5a", Integer.toHexString((int) 'Z'));
+		String str = String.valueOf((char)240);
+		String strHex = new String(Hex.encodeHex(str.getBytes()));
+		System.out.println(str);
+		System.out.println(strHex);
+		System.out.println(Integer.toHexString(240));
+		byte[] b = str.getBytes();
+	}
+	
+	/**
+	 * Method này dùng để test character 240 trong bảng mã ASCII.<br>
+	 * Tham khảo: http://dev.networkerror.org/utf8/
+	 */
+	@Test
+	public void testEncode240(){
+		char c = 'ð';
+		
+		// method chuyển sang số thứ tự trong bảng mã ASCii
+		assertEquals(240, (int)c);
+		
+		// method chuyển sang số thự tụ heximal trong bảng mã ASCII
+		assertEquals("f0", Integer.toHexString((int)c));
+		
+		// method chuyển sang raw hex
+		String str = String.valueOf(c);
+		assertEquals("c3b0", new String(Hex.encodeHex(str.getBytes()))); //c3b0 nghĩa là 1100 0011 1011 0000
+		
+		// method chuyển sang byte array
+		byte[] b = str.getBytes();
+		assertEquals(-61, b[0]); // -61 nghĩa là 1100 0011
+		assertEquals(-80, b[1]); // -80 nghĩa là 1011 0000
+	}
 	
 	@Test
-	public void testBytesToHex(){
+	public void testBytesToHex() {
 		// scenario 1
 		String str = "A";
 		String result = DbcsFunctions.bytesToHex(str.getBytes());
 		String resultApache = new String(Hex.encodeHex(str.getBytes()));
 		assertEquals("41", result);
 		assertEquals("41", resultApache);
-		
+
 		// scenario 2
 		str = "大";
 		result = DbcsFunctions.bytesToHex(str.getBytes());
 		resultApache = new String(Hex.encodeHex(str.getBytes()));
 		assertEquals("E5A4A7", result);
 		assertEquals("e5a4a7", resultApache);
-		
+
 		// scenario 3
 		str = "A大";
 		result = DbcsFunctions.bytesToHex(str.getBytes());
@@ -81,50 +126,81 @@ public class DbcsTest {
 		assertEquals("41E5A4A7", result);
 		assertEquals("41e5a4a7", resultApache);
 	}
-	
+
 	@Test
-	public void testDecodeHex() throws DecoderException{
+	public void testDecodeHex() throws DecoderException {
 		// scenario 1
 		String str = "41";
 		String resultApache = new String(Hex.decodeHex(str.toCharArray()));
-		assertEquals("A", resultApache);	
-		
+		assertEquals("A", resultApache);
+
 		// scenario 2
 		str = "e5a4a7";
 		resultApache = new String(Hex.decodeHex(str.toCharArray()));
 		assertEquals("大", resultApache);
-		
+
 		// scenario 3
 		str = "e5a4a742";
 		resultApache = new String(Hex.decodeHex(str.toCharArray()));
 		assertEquals("大B", resultApache);
 	}
-	
+
+	@Test
+	public void testCheckLeadAndTrailByte() {
+		// scenario 1: c=0x40=@
+		char c = 0x40;
+		assertEquals('@', c);
+		assertTrue(DbcsFunctions.isTrailingChar(c));
+		assertFalse(DbcsFunctions.isDbcsLeadingChar(c));
+
+		// scenario 2: c=0x41=A
+		c = 0x41;
+		assertEquals('A', c);
+		assertTrue(DbcsFunctions.isTrailingChar(c));
+		assertFalse(DbcsFunctions.isDbcsLeadingChar(c));
+
+		// scenario 3: c=0x3d='='
+		c = 0x3d;
+		assertEquals('=', c);
+		assertFalse(DbcsFunctions.isTrailingChar(c));
+		assertFalse(DbcsFunctions.isDbcsLeadingChar(c));
+
+		// scenario 4: c=0x81
+		c = 0x81;
+		assertTrue(DbcsFunctions.isTrailingChar(c));
+		assertTrue(DbcsFunctions.isDbcsLeadingChar(c));
+		
+		// scenario 5: c=0xa1
+		c = 0xa1;
+		assertTrue(DbcsFunctions.isTrailingChar(c));
+		assertFalse(DbcsFunctions.isDbcsLeadingChar(c));
+	}
+
 	/**
 	 * Test DbcsFunctions.containsDbcsScript
 	 */
 	@Test
-	public void TestContainsHanScript(){
+	public void TestContainsHanScript() {
 		String chineseStr = "xxx已下架xxx";
 		boolean result = DbcsFunctions.containsHanScript(chineseStr);
 		assertTrue(result);
-		
+
 		chineseStr = "已下";
 		result = DbcsFunctions.containsHanScript(chineseStr);
 		assertTrue(result);
-		
+
 		chineseStr = "已";
 		result = DbcsFunctions.containsHanScript(chineseStr);
 		assertTrue(result);
-		
+
 		chineseStr = "Thận";
 		result = DbcsFunctions.containsHanScript(chineseStr);
 		assertFalse(result);
-		
+
 		chineseStr = "ư";
 		result = DbcsFunctions.containsHanScript(chineseStr);
 		assertFalse(result);
-		
+
 		chineseStr = " ";
 		result = DbcsFunctions.containsHanScript(chineseStr);
 		assertFalse(result);
