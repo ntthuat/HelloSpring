4624ce4bfdd6a3548275e1dca9eca753d29982ab
ntthuat <nguyenthienthuat92@gmail.com>
2017-08-17 18:13:39 +0700

    08/17/2017 commit

--- springboot-mvc-hibernate-restful-webservices-zero/src/test/java/zero/service/SongServiceTest.java
+++ springboot-mvc-hibernate-restful-webservices-zero/src/test/java/zero/service/SongServiceTest.java
@@ -1,9 +1,15 @@
 package zero.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 
 import zero.model.Song;
 
@@ -34,4 +40,29 @@ public class SongServiceTest {
 		}
     	System.out.println("Thuat");
     }
+    
+    @Test
+    public void testListBean() {
+    	getInstantiatedSigletons(appContext);
+    }
+    
+    /**
+     * @author Thuat T Nguyen
+     * @version 08/03/2017
+     * 
+     * Hàm check các bean được tạo ra
+     */
+    public static List<Object> getInstantiatedSigletons(ApplicationContext ctx) {
+		List<Object> singletons = new ArrayList<Object>();
+
+		String[] all = ctx.getBeanDefinitionNames();
+
+		ConfigurableListableBeanFactory clbf = ((AbstractApplicationContext) ctx).getBeanFactory();
+		for (String name : all) {
+			Object s = clbf.getSingleton(name);
+			if (s != null)
+				singletons.add(s);
+		}
+		return singletons;
+	}
 }
