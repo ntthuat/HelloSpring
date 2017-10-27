f5b80acc1490c453eec593a481a6d0f506fd1a14
ntthuat <nguyenthienthuat92@gmail.com>
2017-10-27 09:01:10 +0700

    10/27/2017 commit at home 2

---
+++ spring-zero/spring-zero/src/main/java/com/springzero/sequencegenerator/example/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Class này làm ví dụ trên mạng về sequence generator.
+ * http://www.logicbig.com/tutorials/java-ee-tutorial/jpa/seq-generator/
+ */
+/**
+ * @author Thuat Nguyen
+ * @version 10/27/2017
+ */
+package com.springzero.sequencegenerator.example;
---
+++ spring-zero/spring-zero/src/main/java/com/springzero/sequencegenerator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package này làm về sequence generator.
+ */
+/**
+ * @author Thuat Nguyen
+ * @version 10/27/2017
+ */
+package com.springzero.sequencegenerator;
