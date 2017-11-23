471db2784ec138195faed9a9f0567b7f104cf987
ntthuat <nguyenthienthuat92@gmail.com>
2017-11-23 08:02:56 +0700

    11/23/2017 morning commit at home

--- spring-zero/spring-zero/src/main/java/com/springzero/javacertificate/z803/Question11.java
+++ spring-zero/spring-zero/src/main/java/com/springzero/javacertificate/z803/Question11.java
@@ -6,12 +6,18 @@ public class Question11 {
 		Tell tellIt = new Tell();
 		/*speakIt.tellItLikeItIs();*/ // this method fail because tellIdLikeItIs do not exist in Speak
 		/*(Truth)speakIt.tellItLikeItIs();*/ // this method fail because tellIdLikeItIs do not exist in Speak
-		((Truth)speakIt).tellItLikeItIs();
+		((Truth)speakIt).tellItLikeItIs(); // it cast to Truth then cast to Tell because of line 5 execute the method
+		/*((Truth2)speakIt).tellItLikeItIs();*/ // có thể cast được ở đây, nhưng chạy sẽ gặp lỗi ClassCastException
+		tellIt.tellItLikeItIs();
+		/*(Truth)tellIt.tellItLikeItIs();*/ // interface không thể cast cho 1 variable (void ở đây tính là variable)
+		((Truth)tellIt).tellItLikeItIs();
 	}
 }
 
 class Speak{
-	
+	public void abc() {
+		System.out.println("abc");
+	}
 }
 
 class Tell extends Speak implements Truth{
@@ -23,8 +29,21 @@ class Tell extends Speak implements Truth{
 	
 }
 
+interface Truth2 {
+	public void tellItLikeItIs();
+}
+
 interface Truth{
 	public void tellItLikeItIs();
+}
+
+class Tell2 extends Speak implements Truth{
+
+	@Override
+	public void tellItLikeItIs() {
+		System.out.println("Right on 2");
+	}
+	
 }
 
 
