d47f376a4368cc676ad59d9d5eb20da99b02a4b9
ntthuat <nguyenthienthuat92@gmail.com>
2017-11-24 12:55:50 +0700

    11/24/2017 commit at home

--- spring-zero/spring-zero/src/main/resources/scripts/employee-mysql.sql
+++ spring-zero/spring-zero/src/main/resources/scripts/employee-mysql.sql
@@ -1,3 +1,4 @@
+-- TABLE EMPLOYEE --
 CREATE TABLE employee (
   ID int(11) NOT NULL,
   NAME varchar(11) NOT NULL
@@ -8,4 +9,18 @@ INSERT INTO employee (ID, `NAME`) VALUES
 (3, 'Thuat'),
 (4, 'Nick');
 ALTER TABLE employee
+  ADD PRIMARY KEY (ID);
+
+-- TABLE STUDENT --
+CREATE TABLE student (
+  ID INT NOT NULL,
+  NAME VARCHAR(45) NOT NULL
+ );
+ 
+ INSERT INTO student (ID, `NAME`) VALUES
+(1, 'John'),
+(2, 'Max'),
+(3, 'Thuat'),
+(4, 'Alistair');
+ALTER TABLE student
   ADD PRIMARY KEY (ID);
