34b08ce7969146b6799ffb78a15073dad12a7337
ntthuat <nguyenthienthuat92@gmail.com>
2017-12-08 16:26:44 +0700

    2017/12/08 commit

---
+++ spring-zero/spring-zero/src/main/java/com/springzero/javacertificate/z803/Question115.java
@@ -0,0 +1,20 @@
+package com.springzero.javacertificate.z803;
+
+public class Question115 {
+	
+	public static void doChange(int... array){
+		for (int i = 0; i < array.length; i++) {
+			array[i] = array[i] + 1;
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] array = {10, 20, 30};
+		doChange(array);
+		for (int i : array) {
+			System.out.println(i+", ");
+		}
+		doChange(array[0], array[1], array[2]);
+		System.out.println(array[0]+", "+array[1]+", "+array[2]+", ");
+	}
+}
--- spring-zero/spring-zero/src/main/java/com/springzero/javacertificate/z803/Question117.java
+++ spring-zero/spring-zero/src/main/java/com/springzero/javacertificate/z803/Question117.java
@@ -2,11 +2,19 @@ package com.springzero.javacertificate.z803;
 
 public class Question117 {
 	private int i;
+
 	void disp() {
-		i++;
-		System.out.println(i);
+		while (i <= 5) {
+			for (int i = 1; i <= 5;) {
+				System.out.print(i + " ");
+				i++;
+			}
+			i++;
+		}
 	}
+
 	public static void main(String[] args) {
 		new Question117().disp();
+		System.out.println("end");
 	}
 }
